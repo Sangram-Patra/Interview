523d0afe092d12567b4cfdbd62737cffd88ad963
Sangram-Patra <sangramkumarp530@gmail.com>
2025-06-17 09:29:46 +0530

    video 1 done

---
+++ Basic/react/app.js
@@ -0,0 +1,7 @@
+const parent = React.createElement(
+  "div",
+  { id: "name", className: "css" },
+  "I am the app js "
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
